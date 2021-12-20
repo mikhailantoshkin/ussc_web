f277ee8db1314616029f3d8b28bfb2897193d2d7
mikhailantoshkin <mike.antoshkin@gmail.com>
2021-12-20 10:42:23 +0500

    error handling

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,34 @@
 use redis;
+use redis::AsyncCommands;
+use rocket::http::Status;
+use rocket::response::status;
+use rocket::response::status::NotFound;
 use rocket::State;
-// fn connect_to_to_redis() -> redis::Connection {
-//    let client = redis::Client::open("redis://127.0.0.1/").unwrap();
-// }
+
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-async fn index(redis_cliednt: &State<redis::Client>) -> &'static str {
-    let conn = redis_cliednt.get_async_connection().await.unwrap();
-    "Hello, world!"
+#[get("/<name>")]
+async fn index(
+    name: &str,
+    redis_client: &State<redis::Client>,
+) -> Result<String, status::Custom<String>> {
+    let mut conn = redis_client
+        .get_async_connection()
+        .await
+        .map_err(|e| status::Custom(Status::ServiceUnavailable, "Dead redis".to_string()))?;
+    let res: String = conn
+        .get("test")
+        .await
+        .map_err(|e| status::Custom(Status::BadRequest, e.to_string()))?;
+    let _: () = conn.set("test", name).await.unwrap();
+    // "Hello, world!"
+    Ok(format!("Hello, {}", res))
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .manage(redis::Client::open("redis://redis/").unwrap())
+        .manage(redis::Client::open("redis://localhost:6379/").unwrap())
         .mount("/", routes![index])
 }
